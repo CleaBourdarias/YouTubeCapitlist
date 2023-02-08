5b6cf7e76173dc2521254c65fc3fb0e05d71e8d5
CleaBourdarias <clea.bourdarias@orange.fr>
2023-02-08 18:04:22 +0100

    gestion mondes users

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -1,9 +1,33 @@
+const fs = require("fs").promises
+
+function saveWorld(context) {
+    fs.writeFile("userworlds/" + context.user + "-world.json",
+        JSON.stringify(context.world), err => {
+            if (err) {
+                console.error(err)
+                throw new Error(
+                    `Erreur d'écriture du monde coté serveur`)
+            }
+        })
+};
+
+
 module.exports = {
     Query: {
-        getWorld(parent, args, context) {
+        getWorld(parent, args, context, info) {
+            saveWorld(context)
             return context.world
         }
     },
     Mutation: {
+        acheterQtProduit(parent, args, context){
+            let produit = context.world.products.find(p => p.id === args.id)
+
+            if (produit === undefined){
+                throw new Error(
+                    `Le produit avec l'id ${args.id} n'existe pas`)
+            }else{}
+        }
     }
 };
+
